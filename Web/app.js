c6cb90fd704ae193cdb77d9dff9844016de4f6c7
GGS92carmnad <goldengrizzlyservers@gmail.com>
2021-05-27 11:52:51 +0100

    Added Pug render engine
    Set up our backend to use Pug to render dynamic web pages

--- Web/app.js
+++ Web/app.js
@@ -4,15 +4,15 @@ const path = require('path');
 var app = express();
 var port = 3000;
 
+app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, '/views/'));
 
 app.get('/', (req, res) => {
     res.send(`
         <h1>Test index page</h1>
-        <h3>Click <a href="/database">here</a> for access to the db</h3>
     `);
 });
 
 app.listen(port, () => {
-    console.log('Express listening of port ' + port)
+    console.log('Express listening on port ' + port)
 });
